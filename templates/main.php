5a02142b46163d8796010dddd951fc888009ebf6
Eowyn Cenek <eowyn.cenek@gmail.com>
2015-10-01 08:56:27 -0500

    Revert "Corrected main page"
    This reverts commit fb8e6dd2f581518797be98e0638eede63b4db9c6.

--- templates/main.php
+++ templates/main.php
@@ -28,7 +28,6 @@ while ($row = mysqli_fetch_assoc($result)) {
 echo "</br>";
 
 /* query: select a person's awards in the database in the db */
-<!--
 $query = "SELECT name_person, name_award, date_award from Persons, Awards_Persons, Awards
    WHERE Persons.id_person = Awards_Persons.id_person
          and Awards_Persons.id_award = Awards.id_award
@@ -39,7 +38,7 @@ while ($row = mysqli_fetch_assoc($result))
   {extract($row);
   echo "<b>$name_person</b> was awarded <i>$name_award</i> on $date_award. <br/>";
 };
--->
+
 
 mysqli_close ($cxn); /* close the db connection */
 ?>
