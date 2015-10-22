3593512371dee8c34570952a23bc455c31308833
Jacqueline Arsenault <justcordelya@gmail.com>
2015-10-21 21:10:57 -0500

    Continued Work on Login Script
    Some progress made in making the login script minimally serviceable. The
    query is failing. The page does render the login form. Bits that are not
    ready are commented out.

--- public/login.php
+++ public/login.php
@@ -15,13 +15,14 @@
         {
             apologize("You must provide your password.");
         }
+
+        $username = $_POST["username"];
         // connect to the db
         $cxn = mysqli_connect (SERVER,USERNAME,PASSWORD,DATABASE)
         or die ("message");
 
-        $query = "SELECT * FROM WebUsers WHERE name_webuser = ?", $_POST["username"]);
-        //TODO we need to select more data than just the row from WebUsers.
-        // We also need all rows from Webusers_Roles that are associated with the user's id
+        $query = "SELECT * FROM webusers WHERE name_webuser = $username";
+
 
         // query database for user
         $rows = mysqli_query ($cxn, $query)
@@ -38,23 +39,24 @@
             if (crypt($_POST["password"] . SALT) == $row["password_webuser"])
             {
 
-              // regenerate the session_id because we are changing the level of
-              // privilege here
-              if (!isset($_SESSION['initiated']))
-              {
-                session_regenerate_id();
-                $_SESSION['initiated'] = true;
-              }
+            // regenerate the session_id because we are changing the level of
+            // privilege here
+            if (!isset($_SESSION['initiated']))
+            {
+              session_regenerate_id();
+              $_SESSION['initiated'] = true;
+            }
 
-                // remember that user is now logged in by storing user's ID in session
-                $_SESSION["id"] = $row["id_webuser"];
+              // remember that user is now logged in by storing user's ID in session
+              $_SESSION["id"] = $row["id_webuser"];
 
-                // generate a key based on user_agent, user ID,
+              // generate a key based on user_agent, user ID,
 
-                $_SESSION["key"] = NULL // we need to come up with a unique session key
-                // built from various bits relevant to the session, such as HTTP_USER_AGENT
-                // and potentially also the IP address, though this can cause problems
-                // with users browsing via TOR or via an IP pool/load balancer
+              $_SESSION["key"] = random_int(0, 999999); // we need to come up with a unique session key
+              // built from various bits relevant to the session, such as HTTP_USER_AGENT
+              // and potentially also the IP address, though this can cause problems
+              // with users browsing via TOR or via an IP pool/load balancer
+              // in the mean time, a random number will do.
 
 
               // TODO: insert additional $_SESSION variables for determining
@@ -63,11 +65,40 @@
               // $_SESSION["marshal_rapier"] = $row["marshal_rapier"] (stores a TRUE/FALSE value)
               // etc, etc, so we can test against these variables to determine if a user
               // should be able to modify the data.
+              /* I began writing this section, however I need to do some more
+                studying on how to obtain both the list of role types and the
+                list of webuser roles for a specific webuser. I may be overthinking this.
+
+              $cxn = mysqli_connect (SERVER,USERNAME,PASSWORD,DATABASE)
+              or die ("message");
+
+              $query = "SELECT * FROM webusers_roles, roles WHERE (`webusers_roles`.* id_webuser == ?)", $_SESSION["id"]);
+
+              // query database for user
+              $rows = mysqli_query ($cxn, $query)
+              or die ("Couldn't execute query");
+
+              // test the results for current matches
+              foreach ($rows as $row) {
+                $expire = (strtotime($row["role_expire"]) - time()); // calc diff btwn role_expire and now
+                if ($expire >= 0)                                    // non-expired == positive diff
+                {
+                  foreach($rows as $role) //We're looking for results in id_role and id_roletype columns
+                  {
+                    if $row[] //brain has gone to mush and I stopped here and commented the entire section
+                  }
+                }
+
+              }
+
+              */
 
               // TODO: set expirations. Need: destroy on browser close; expire after
               // 14 days, if (using a public computer) {expire after 4 hours}
               // based on login form input -> tickbox asking, "Are you using a shared or
               // public computer?". If TRUE, expire after 4 hours, else, expire after 2 weeks
+
+
 
                 // redirect to main page
                 redirect("/");
