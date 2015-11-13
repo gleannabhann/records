94e62434da75cd78558e078ab28fa07022048fd3
Jacqueline Arsenault <justcordelya@gmail.com>
2015-11-12 19:45:25 -0600

    Edit_Site Form Fields; list_site table formatting
    Majority of work done to edit_site.php. Added in fields for all table
    fields. Textarea fields for fields expected to contain more text than
    the 256 char width. Set "Verified" to default to display "never" if it
    is not already set to something else. TODO: the previously listed item
    will need some testing to make sure it works correctly once we have some
    updated records with date/time stamps in the verified column.

--- templates/list_site.php
+++ templates/list_site.php
@@ -3,67 +3,73 @@
 /* connect to the database */
 $cxn = open_db_browse();
 /*#######################################################################################*/
-// This will list all the sites in the database.  
-// If the user is logged in with the Sites rolytype, will also include edit/delete/add buttons
+// This will list all the sites in the database.
+// If the user is logged in with the Sites roletype, will also include edit/delete/add buttons
 echo "<div class='page-header'><h1>Sites Sorted by State</h1><small>";
 echo "</small></div>"; //Customize the page header
+
 
 //echo "is_logged_in() returns ". is_logged_in()."<br>";
 //echo "permissions(Sites) returns " . permissions("Sites")."<br>";
 
-$query = "select @rn:=@rn+1 as row_number, s.* from Sites s, (SELECT @rn:=0) r order by active_site desc;";
+$query = "select @rn:=@rn+1 as row_number, s.* from Sites s, (SELECT @rn:=0) r;";
 $result = mysqli_query ($cxn, $query)
 or die ("Couldn't execute query");
 echo "
 <div class='row'>
 
   <div class='col-md-8 col-md-offset-2'>";
-echo "<table class='table table-condensed table-bordered'>
+// table header without width percentages
+  echo "<table class='table table-bordered'>
+  <thead>
+  <td class='text-left'><strong>&nbsp;</strong></td>
+  <td class='text-left'><strong>Name</strong></td>
+  <td class='text-left'><strong>Facilities</strong></td>
+  <td class='text-left'><strong>Capacity</strong></td>
+  <td class='text-left'><strong>Cost</strong></td>
+  <td class='text-left'><strong>Area</strong></td>
+  <td class='text-left'><strong>Contact</strong></td>";
+
+// table header with width percentages.
+/*
+echo "<table class='table table-bordered'>
 <thead>
-<td class='text-left' style=\"width:5%\"><strong>Site</strong></td>
 <td class='text-left' style=\"width:15%\"><strong>Name</strong></td>
 <td class='text-left' style=\"width:25%\"><strong>Facilities</strong></td>
 <td class='text-left' style=\"width:5%\"><strong>Capacity</strong></td>
 <td class='text-left' style=\"width:15%\"><strong>Cost</strong></td>
 <td class='text-left' style=\"width:20%\"><strong>Area</strong></td>
-<td class='text-left' style=\"width:10%\"><strong>Contact</strong></td>";
-// TODO: replace is_logged_in() with is_site_admin() permissions check
+<td class='text-left' style=\"width:10%\"><strong>Contact</strong></td>"; */
+
+
+
 if (permissions("Sites") >= 3){
-    "<td class='text-left' style=\"width:5%\"><strong></strong></td>";
+echo    "<td class='text-left' style=\"width:5%\"><strong>&nbsp;</strong></td>";
 };
 echo " </thead>";
 
 while ($row = mysqli_fetch_assoc($result)) {
     extract($row);
-    if (($active_site > 0) || (permissions("Sites") >= 3)) {
-        //TODO: Indicate if site is inactive
-        echo "<tr>";
-        echo "<td class='text-left' style=\"width:5%\"> $row_number</td>";
-        if ($active_site) {
-                echo "<td class='text-left' style=\"width:15%\">$name_site";}
-            else {
-                echo "<td class='text-left' style=\"width:15%\">$name_site (INACTIVE)";
-            }   
-        if ($url_site !="") echo "<a href=\"$url_site\">Link</a>";
-        echo "</td>";
-        echo "<td class='text-left' style=\"width:25%\">$facilities_site</td>";
-        echo "<td class='text-left' style=\"width:5%\"> $capacity_site</td>";
-        echo "<td class='text-left' style=\"width:15%\">$rates_site</td>";
-        echo "<td class='text-left' style=\"width:20%\">$area_site</td>";
-        echo "<td class='text-left' style=\"width:10%\">$contact_site</td>";
-        if (permissions("Sites") >= 3){
-            echo "<td class='text-left' style=\"width:5%\">
-            <a href=\"./edit_site.php?id=$id_site\">Edit</a> &nbsp <a href=\"\">Delete</a>
-            </td>";
-        };
-        echo "</tr>";
-    }
+    echo "<tr>
+    <td class='text-left' style=\"width:5%\"> $row_number</td>
+    <td class='text-left' style=\"width:15%\">$name_site</td>
+    <td class='text-left' style=\"width:25%\">$facilities_site</td>
+    <td class='text-left' style=\"width:5%\"> $capacity_site</td>
+    <td class='text-left' style=\"width:15%\">$rates_site</td>
+    <td class='text-left' style=\"width:20%\">$area_site</td>
+    <td class='text-left' style=\"width:10%\">$contact_site</td>";
+    if (permissions("Sites") >= 3){
+        echo "<td class='text-left' style=\"width:5%\">
+        <a href=\"./edit_site.php?id=$id_site\">Edit</a>
+        </td>";
+    };
+    echo "</tr>";
 }
 
 echo "</table>";
 echo "</div><!-- ./col-md-8 --></div><!-- ./row -->"; //close out list and open divs
-echo "<hr><p>Browse by Name:</p><p>";#######################################################################################*/
+// echo "<hr><p>Browse by Name:</p><p>"; // future alpha sort header
+#######################################################################################*/
 mysqli_close ($cxn); /* close the db connection */
 ?>
 </div>
-
