42db1342ba1b9deadb2fed9c3b457fe3ea4dc147
Jacqueline Arsenault <justcordelya@gmail.com>
2015-12-09 18:44:23 -0600

    Add discrepancy reporting form to site.php
    Visitors may now submit feedback on the accuracy of individual
    campground records. Needs testing on destination server.

--- templates/site.php
+++ templates/site.php
@@ -150,10 +150,102 @@ echo "<div class='row'><div class='col-md-6 col-md-offset-3'>";
 
 
 echo "</div><!-- ./col-md-6 --></div><!-- ./row -->"; //close out list and open divs
+
+
+// If the submit button was pressed, handle the email.
+if (isset($_POST["msgSubmit"])) {
+    //TODO: Need to filter these fields carefully.
+  $name = $_POST['name'];
+  $email = $_POST['email'];
+  $msgBody = wordwrap($_POST['msgBody']);
+  $from = 'forms@oopgleannabhann.net';
+  $to = 'webminister@gleannabhann.net';
+//  $to = 'webminister@gleannabhann.net' . ', ';
+//  $to .= 'obsidian@gleannabhann.net';
+  $subject = $_POST['subject'];
+  $body = "From: $name\n Email: $email\n Message:\n $msgBody";
+
+  // check for name
+  if (!$_POST['name']) {
+    $errName = "Please enter your name";
+  } else {$errName = false;}
+  if (!$_POST['email']) {
+    $errEmail = "Please enter your email address";
+  } else {$errEmail = false;}
+  if (!$_POST['msgBody']) {
+    $errMessage = "Please enter information about the discrepancy or error";
+  } else {$errMessage = false;}
+  if (!$errName && !$errEmail && !$errMessage) {
+     if (mail ($to, $subject, $body, $from)) {
+       $emailresult = '<div class="alert alert-success">Thank you! We appreciate your feedback.</div>';
+     } else {
+       $emailresult ='<div class="alert alert-danger">I was unable to send your message. Please try again.</div>';
+     }
+   } else {
+       echo "Error with setting up email.";
+   }
+}
+
+
+
+
+
+
 #######################################################################################*/
 mysqli_close ($cxn); /* close the db connection */
 
 ?>
+<!-- end of php -->
+<div class="row">
+  <?php echo form_title("Report a problem with this record") ?>
+  <form class="form-horizontal" role="form" method="post" action="site.php">
+    <div class="form-group">
+      <label for="name" class="col-sm-2 col-md-3 control-label">Name:</label>
+      <div class="input-group col-sm-10 col-md-6">
+        <span style="display: block; width: 100%"><input size="60" type="text" width="100%" class="form-control" id="name" name="name" placeholder="Your Name" value="<?php if (isset($_POST['msgSubmit'])) {echo htmlspecialchars($_POST['name']);} ?>"></span>
+      </div>
+    </div>
+    <div class="form-group">
+      <label for="email" class="col-sm-2 col-md-3 control-label">Email:</label>
+      <div class="input-group col-sm-10 col-md-6">
+  <input size="60" type="text" width="100%" class="form-control" id="email" name="email" placeholder="example@domain.com" value="<?php if (isset($_POST['msgSubmit'])) {echo htmlspecialchars($_POST['email']);} ?>">
+
+      </div>
+    </div>
+    <div class="form-group">
+      <label for="subject" class="col-sm-2 col-md-3 control-label">Subject:</label>
+      <div class="input-group col-sm-10 col-md-6">
+        <input size="60" type="text" class="form-control" id="subject" name="subject"
+             value="<?php
+                         echo "Record correction for $name_site (ID $id_site)";
+                    ?>">
+      </div>
+    </div>
+    <div class="form-group">
+      <label for="msgBody" class="col-sm-2 col-md-3 control-label">Details:</label>
+      <div class="input-group col-sm-10 col-md-6">
+          <textarea cols="62" class="form form-control" rows="4" name="msgBody" placeholder="Tell us what's incorrect about this record." id="msgBody" value="<?php if (isset($_POST['msgSubmit'])) {echo htmlspecialchars($_POST['msgBody']);} ?>">
+        </textarea>
+
+      </div>
+    </div>
+
+    <div class="form-group">
+      <div class="input-group col-sm-10 col-sm-offset-2 col-md-6 col-md-offset-3">
+          <input id="msgSubmit" name="msgSubmit" type="submit" value="Send Report">
+        <input type="hidden" name="id"
+             value="<?php
+                         echo "$id_site";
+                    ?>">
+         </div>
+    </div>
+    <div class="form-group">
+      <div class="input-group col-sm-10 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <?php if(isset($emailresult)) {echo $emailresult;} ?>
+      </div>
+    </div>
+  </form>
+</div>
 
 <script>
 var oldWindow;
