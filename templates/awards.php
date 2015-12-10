c05a851378e25ccb9d98054d23386a68e8295c80
Jacqueline Arsenault <justcordelya@gmail.com>
2015-12-09 18:30:53 -0600

    Page title text modification
    Modified the information that gets placed into <title> in the header
    template to better describe the purpose of each page.

--- templates/awards.php
+++ templates/awards.php
@@ -1,82 +1,9 @@
 <?php
-/* connect to the database */
-$cxn = open_db_browse();
 
-echo "<div class='row'><div class='col-md-6 col-md-offset-3'>";
+    // configuration
+    require("../includes/config.php");
 
+    // render portfolio
+    render("awards.php", ["title" => "Awards"]);
 
-///////////////////////////////////////////////////////////////////////////////
-// Main portion of the page
-///////////////////////////////////////////////////////////////////////////////
-echo "<div class='center-block'>";
-include "alpha.php";
-echo "</div>";
-echo "<div class='center-block'>";
-echo form_subtitle("Search on a Partial Name");
-    echo '<form role="search" action="search.php" method="get">';
-    echo  '<input type="text" class="form-control" '
-    . 'placeholder="Search for Name or Award" name="name">';
-    echo '<button type="submit" class="btn btn-default">Submit</button>';
-    ///////////////////////////////////////////////////////////////////////////////
-    // Include the most recent 5 events
-    ///////////////////////////////////////////////////////////////////////////////
-    echo "<br><br>";
-    echo form_subtitle("The 6 most recent events:");
-    echo button_link("list_events.php", "List all events in the database");
-    $query = "SELECT id_event, name_event, name_group, date_event_start, date_event_stop "
-            . "FROM Events, Groups "
-            . "WHERE Events.id_group = Groups.id_group "
-            . "AND date_event_stop <= curdate() "
-            . "ORDER BY date_event_start DESC "
-            . "LIMIT 6;";
-    $result = mysqli_query ($cxn, $query) or die ("Couldn't execute event_info query");
-    
-    echo "<table class='table table-condensed table-bordered'>
-    <thead>
-    <td ><strong>Event</strong></td>
-    <td class='text-left'><strong>Hosts</strong></td>
-    <td class='text-left'><strong>Dates</strong></td>
-    </thead>";
-    while ($row = mysqli_fetch_assoc($result)){
-        extract($row);
-        echo "<tr>";
-        echo "<td ><a href='event.php?id=$id_event'>$name_event</a></td>";
-        echo "<td >$name_group</td>";
-        echo "<td >".date("d-M-Y", strtotime($date_event_start));
-        if ($date_event_start != $date_event_stop) {
-            echo " --  ".date("d-M-Y", strtotime($date_event_stop));
-        }
-        echo "</td>";
-        echo "</tr>";
-    }
-    echo "</table>";
-    
-echo "</div>";
-echo "</div><!-- ./col-md-6 -->";
-
-///////////////////////////////////////////////////////////////////////////////
-// News column on right
-///////////////////////////////////////////////////////////////////////////////
-echo '<div class="col-md-3 well">';
-echo form_subtitle("Most recent awards");
-$query = "SELECT Persons.id_person, name_person, name_award, date_award, Awards.id_award "
-        . "FROM Persons, Persons_Awards, Awards "
-        . "WHERE Persons.id_person=Persons_Awards.id_person "
-        . "AND Awards.id_award = Persons_Awards.id_award "
-        . "ORDER BY date_award DESC "
-        . "LIMIT 20";
-$result = mysqli_query ($cxn, $query) or die ("Couldn't execute query");
-while ($row = mysqli_fetch_assoc($result)) {
-    extract($row);
-    echo "<li><a href='person.php?id=$id_person'>"
-            . "$name_person</a> received <a href='list.php?award=$id_award'>$name_award</a> on $date_award"
-            . "</li>";
-}
-
-include "warning.php"; // includes the warning text about paper precedence
-
-echo "<div> <!-- ./col-md-3 --></div> <!-- ./row -->";
-
-
-mysqli_close ($cxn); /* close the db connection */
 ?>
--- templates/list_events.php
+++ templates/list_events.php
@@ -1,51 +1,10 @@
-<div class="container">
 <?php
-/* Note: This page displays a list of events.  If permissions are correct, then 
- * the page will also include an "Add Event" button, and the links will go to 
- * edit_event.php rather than event.php.
- */
-$cxn = open_db_browse();
 
-$query = "SELECT id_event, name_event, name_group, date_event_start, date_event_stop "
-        . "FROM Events, Groups "
-        . "WHERE Events.id_group = Groups.id_group "
-        . "ORDER BY date_event_start DESC;";
-if (DEBUG){
-    echo "Event Information Query is:<p>$query<p>";
-}
-$result = mysqli_query ($cxn, $query) or die ("Couldn't execute event_info query");
+    // configuration
+    require("../includes/config.php");
 
-echo "<div class='row'><div class='col-md-8 col-md-offset-2'>";
-echo form_title("A List of All Known Events");
-if (permissions("Herald")>= 3){
-    echo button_link("add_event.php","Add A New Event");
-}
-echo "</p>";
-echo "<table class='table table-condensed table-bordered'>
-<thead>
-<td ><strong>Event</strong></td>
-<td class='text-left'><strong>Hosts</strong></td>
-<td class='text-left'><strong>Dates</strong></td>
-</thead>";
-while ($row = mysqli_fetch_assoc($result)){
-    extract($row);
-    echo "<tr>";
-    echo "<td ><a href='event.php?id=$id_event'>$name_event";
-    echo "</a></td>";
-    echo "<td >$name_group</td>";
-    echo "<td >".date("d-M-Y", strtotime($date_event_start));
-    if ($date_event_start != $date_event_stop) {
-        echo " --  ".date("d-M-Y", strtotime($date_event_stop));
-    }
-    echo "</td>";
-    if (permissions("Herald")>= 3){
-        echo "<td>".button_link("edit_event.php?id=$id_event", "Edit Event")."</td>";
-    }
-    echo "</tr>";
-}
-echo "</table>";
-echo "</div><!-- ./col-md-8 --></div><!-- ./row -->"; //close out list and open divs
+    // Note: can't use ROOTDIR here because ROOTDIR is set in constants.php
+    // render portfolio
+    render("list_events.php", ["title" => "Events"]);
 
-mysqli_close ($cxn); /* close the db connection */
 ?>
-</div>
