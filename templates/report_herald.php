965b1fffa614fc25150c2dc7e136a5136991b483
Eowyn <webminister@gleannabhann.net>
2016-02-25 12:56:41 -0600

    Added one simple report form for heralds.

--- templates/report_herald.php
+++ templates/report_herald.php
@@ -19,10 +19,51 @@ if ($_SERVER['REQUEST_METHOD'] != 'POST') { // this page wasn't reached through 
 $cxn = open_db_browse(); // Open the db connection which is now live for the subforms
 // Generate the report.
 
-// Build the query based on the parameters
-
+// Build the query based on the parameters: this will be a massive if statement.
+$report=$_POST["id_report"];
+switch ($report) {
+    case "1": // Obsidian report
+        $report_name = "List of all Awards awarded";
+        $query = "select name_person as Name, name_award as Award, date_award as 'Date Awarded',
+                        name_group as 'Group', name_kingdom as Kingdom 
+                    from Persons, Awards, Groups, Kingdoms, Persons_Awards
+                    where Persons_Awards.id_person = Persons.id_person
+                    and Persons_Awards.id_award = Awards.id_award
+                    and Persons.id_group = Groups.id_group
+                    and Awards.id_kingdom = Kingdoms.id_kingdom
+                    order by name_person, name_award;";
+        break;
+    default:
+        echo '<p class="error"> No report selected.</p>';
+        exit_with_footer();        
+}
 // Query the database
+if (DEBUG) {
+    echo "Report query is: $query<p>";
+}
+$data = mysqli_query ($cxn, $query) 
+        or die ("Couldn't execute query to build report.");
 
 // If requested as a file, build the file, else display in a table with sortable columns
-
+if (isset($_POST["get_file"])) {
+    // build file and offer for download
+} else {
+    // Display data
+    echo form_title($report_name);
+    $fields = mysqli_fetch_fields($data);
+    echo "<table class='table table-condensed table-bordered' id=\"sortedtable\">";
+    echo '<thead>';
+        foreach ($fields as $field) {
+            echo '<th>'.$field->name.'</th>';
+        }
+        echo '</thead>';
+    while ($row = mysqli_fetch_assoc($data)) {
+        echo '<tr>';
+        foreach ($row as $field) {
+            echo '<td>'.$field.'</td>';
+        }
+        echo '</tr>';
+    }
+    echo '</table>';
+}
 mysqli_close ($cxn); // Close the db connection
