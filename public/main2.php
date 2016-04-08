0e38135119c0d1b004ea91873f37fcadc54a6cd8
Eowyn <webminister@gleannabhann.net>
2016-04-08 10:13:44 -0500

    Added main development pages to dev branch.

---
+++ public/main2.php
@@ -0,0 +1,6 @@
+<?php
+    // configuration
+    require("../includes/config.php");
+    // render portfolio
+    render("main2.php", ["title" => "Alternate Main Page"]);
+?>
---
+++ templates/main2.php
@@ -0,0 +1,98 @@
+v>
+  <div class="jumbotron">
+
+<div class="row">
+<div class="col-lg-3 col-md-3 col-xs-12">
+    <h2 class="text-center">Awards</h2>
+</div>
+<div class="col-lg-7 col-md-7 col-xs-12">
+    <p class="text-justify"><small>This paragraph is all about awards and people. Lorem
+      ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+      incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+      nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</small>
+      </p>
+</div>
+</div>
+<div class="row">
+  <div class="hidden-lg hidden-md hidden-sm col-xs-12"><h2 class="text-center">Marshals</h2></div>
+<div class="col-lg-7 col-lg-offset-2 col-md-7 col-md-offset-2 col-xs-12">
+
+    <p class="text-justify"><small>This paragraph is all about Marshals and Authorizations. Lorem
+      ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+      incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+      nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</small>
+      </p>
+    </div>
+    <div class="col-lg-3 col-md-3 hidden-xs">
+      <h2 class="text-center">Marshals</h2>
+</div>
+</div>
+<div class="row">
+<div class="col-lg-3 col-md-3 col-xs-12">
+    <h2 class="text-center">Campgrounds</h2>
+  </div>
+  <div class="col-lg-7 col-md-7 col-xs-12">
+    <p class="text-justify"><small>This paragraph is all about campgrounds. Lorem
+      ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+      incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+      nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </small>
+      </p>
+</div>
+</div>
+<div class="row">
+  <div class="hidden-lg hidden-md hidden-sm col-xs-12">
+<h2 class="text-center">About Us</h2>
+  </div>
+<div class="col-lg-7 col-lg-offset-2 col-md-7 col-md-offset-2 col-xs-12">
+
+    <p class="text-justify"><small>This paragraph is all about the developers and
+      maintainers of this site. Lorem
+      ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+      incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+      nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</small>
+      </p>
+</div>
+<div class="col-lg-3 col-md-3 hidden-xs">
+      <h2 class="text-center">About Us</h2>
+</div>
+</div>
+</div>
+
+</div><!-- ./jumbotron -->
+<div class="row">
+  <div class="col-md-10 col-md-offset-1">
+    <div class="col-md-4">
+      <div class="panel panel-default">
+        <div class="panel panel-body">
+        <h2 class="text-center">Awards</h2>
+        <p class="text-justify">Search for or browse awards, groups, and award
+          recipients. See which awards an individual has recieved. See which
+          individuals have received a particular award.</p>
+        <p><a class="btn btn-default" href="/public/awards.php" role="button">View Awards &raquo;</a></p>
+      </div>
+    </div>
+  </div>
+  <div class="col-md-4">
+    <div class="panel panel-default">
+      <div class="panel-body">
+      <h2 class="text-center">Authorizations</h2>
+        <p class="text-justify">Search for or browse authorizations. See which
+          authorizations a fighter holds. See lists of fighters holding active
+        authorizations in each class. See lists of active marshals.</p>
+        <p><a class="btn btn-default" href="#" role="button">View Authorizations &raquo;</a></p>
+      </div>
+    </div>
+  </div>
+  <div class="col-md-4">
+    <div class="panel panel-default">
+      <div class="panel-body">
+        <h2 class="text-center">Campsites</h2>
+        <p class="text-justify">Browse a list of campgrounds within the Kingdom
+        of Gleann Abhann which may be available to rent for Kingdom or Local
+        events. See details about the amenities each campground has to offer.</p>
+        <p><a class="btn btn-default" href="/public/list_site.php" role="button">View Campgrounds &raquo;</a></p>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
