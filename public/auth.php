a6ae6f671e675a11d53f99a334fdd602ca9ba760
Jacqueline Arsenault <justcordelya@gmail.com>
2016-05-02 22:00:01 -0500

    Add printable authorization card
    This page produces a printer-friendly authorization card. $_GET data of
    ?id=xxx must be provided, where xxx is the id number of the individual
    whose card you want to produce. If errors involving authorizations not
    displaying when they should, most likely place to check first is
    spelling of auth or warrant name in code vs spelling in DB, as this file
    uses strcmp operations to determine whether or not to store a value in
    $authorizations or $warrants.

---
+++ public/auth.php
@@ -0,0 +1,254 @@
+<!doctype html>
+<META HTTP-EQUIV="CACHE-CONTROL" CONTENT="NO-CACHE">
+<meta http-equiv="expires" content="0" />
+<html lang="en">
+<head>
+  <title>  </title>
+  <link rel="stylesheet" href="/css/auth.css">
+</head>
+<body>
+<?php
+// configuration
+require("../includes/config.php");
+
+// Initialize Variables
+$sca_name = NULL;
+$mundane_name = NULL;
+$expire_marshal = NULL;
+$expire_authorize = NULL;
+$issue_date = NULL;
+$exp_date = NULL;
+$authorizations = array();
+$warrants = array();
+$mem_number = NULL;
+$mem_exp = NULL;
+
+
+/* connect to the database */
+$cxn = open_db_browse();
+	$itl = "<span class=\"italics\">";
+	$itlx = "</span>";
+	$ctr = "<span class=\"center\">";
+	$ctrx = "</span>";
+	$id_person = $_GET["id"];
+  $id_combat = 1;
+
+// get authorizations
+$query_auth="SELECT A.id_auth, A.name_auth, A.id_combat, PA.id_person_auth
+FROM Authorizations A LEFT JOIN Persons_Authorizations PA
+ON A.id_auth=PA.id_auth AND id_person=$id_person
+WHERE A.id_combat=$id_combat";
+  $result = mysqli_query ($cxn, $query_auth) or die ("Couldn't execute event_info query");
+
+  while ($row = mysqli_fetch_assoc($result)){
+      extract($row);
+      if (($row["name_auth"] == "Rapier") && ($row["id_person_auth"] != NULL))
+        {
+
+            $authorizations[] = "Rapier";
+        }
+      if (($row["name_auth"] == "Secondary") && ($row["id_person_auth"] != NULL))
+        {
+
+            $authorizations[] = "Secondary";
+        }
+      if (($row["name_auth"] == "Spear") && ($row["id_person_auth"] != NULL))
+        {
+
+            $authorizations[] = "Spear";
+        }
+      if (($row["name_auth"] == "Cut and Thrust") && ($row["id_person_auth"] != NULL))
+        {
+
+            $authorizations[] = "Cut and Thrust";
+        }
+      if (($row["name_auth"] == "Rapier (Y)") && ($row["id_person_auth"] != NULL))
+        {
+
+            $authorizations[] = "Rapier (Y)";
+        }
+      if (($row["name_auth"] == "Rapier Secondary (Y)") && ($row["id_person_auth"] != NULL))
+        {
+
+            $authorizations[] = "Rapier Secondary (Y)";
+        }
+      if (($row["name_auth"] == "Combat Archery") && ($row["id_person_auth"] != NULL))
+        {
+
+            $authorizations[] = "Combat Archery";
+        }
+      if (($row["name_auth"] == "Armored Combat") && ($row["id_person_auth"] != NULL))
+        {
+
+            $authorizations[] = "Armored Combat";
+        }
+      if (($row["name_auth"] == "Siege Engineer") && ($row["id_person_auth"] != NULL))
+        {
+
+            $authorizations[] = "Siege Engineer";
+        }
+      if (($row["name_auth"] == "Fibreglass Spear") && ($row["id_person_auth"] != NULL))
+        {
+
+            $authorizations[] = "Fibreglass Spear";
+        }
+}
+
+// get marshal information
+
+$query_marshal="SELECT M.id_marshal, M.name_marshal, M.id_combat, PM.id_person_marshal
+FROM Marshals M LEFT JOIN Persons_Marshals PM
+ON M.id_marshal=PM.id_marshal AND id_person=$id_person
+WHERE M.id_combat=$id_combat";
+
+$result = mysqli_query ($cxn, $query_marshal) or die ("Couldn't execute event_info query");
+
+while ($row = mysqli_fetch_assoc($result)){
+    extract($row);
+
+    if (($row["name_marshal"] == "Rapier Marshal in Training") && ($row["id_person_marshal"] != NULL))
+      {
+
+          $warrants[] = "Rapier Marshal in Training";
+      }
+    if (($row["name_marshal"] == "Rapier Marshal") && ($row["id_person_marshal"] != NULL))
+      {
+
+          $warrants[] = "Rapier";
+      }
+    if (($row["name_marshal"] == "Group Rapier Marshal") && ($row["id_person_marshal"] != NULL))
+      {
+
+          $warrants[] = "Group Rapier";
+      }
+    if (($row["name_marshal"] == "Rapier Authorization Marshal") && ($row["id_person_marshal"] != NULL))
+      {
+
+          $warrants[] = "Rapier Auth";
+      }
+    if (($row["name_marshal"] == "Youth Rapier Marshal") && ($row["id_person_marshal"] != NULL))
+      {
+
+          $warrants[] = "Youth Rapier";
+      }
+    if (($row["name_marshal"] == "Cut and Thrust Marshal") && ($row["id_person_marshal"] != NULL))
+      {
+
+          $warrants[] = "Cut and Thrust";
+      }
+    if (($row["name_marshal"] == "Cut and Thrust Authorization Marshal") && ($row["id_person_marshal"] != NULL))
+      {
+
+          $warrants[] = "Cut and Thrust Auth";
+      }
+    if (($row["name_marshal"] == "Combat Archery Authorization Marshal") && ($row["id_person_marshal"] != NULL))
+      {
+
+          $warrants[] = "Combat Archery Auth";
+      }
+    if (($row["name_marshal"] == "Armored Combat Marshal") && ($row["id_person_marshal"] != NULL))
+      {
+
+          $warrants[] = "Armored Combat";
+      }
+    if (($row["name_marshal"] == "Fiberglass Spear Authorization Marshal") && ($row["id_person_marshal"] != NULL))
+      {
+
+          $warrants[] = "Fiberglass Spear Auth";
+      }
+    if (($row["name_marshal"] == "Siege Marshal") && ($row["id_person_marshal"] != NULL))
+      {
+
+          $warrants[] = "Siege";
+      }
+    if (($row["name_marshal"] == "Siege Authorization Marshal") && ($row["id_person_marshal"] != NULL))
+      {
+
+          $warrants[] = "Siege Auth";
+      }
+
+}
+
+// get personal information
+
+$query_fighter="SELECT name_person, name_mundane_person, membership_person, membership_expire_person, expire_authorize, expire_marshal
+FROM Persons, Persons_CombatCards
+WHERE Persons.id_person = Persons_CombatCards.id_person
+AND id_combat=$id_combat
+AND Persons.id_person=$id_person";
+
+$result = mysqli_query ($cxn, $query_fighter) or die ("Couldn't execute event_info query");
+
+while ($row = mysqli_fetch_assoc($result)){
+    extract($row);
+
+    $sca_name = $row["name_person"];
+    $mundane_name = $row["name_mundane_person"];
+    $expire_marshal = $row["expire_marshal"];
+    $expire_authorize = $row["expire_authorize"];
+    $mem_number = $row["membership_person"];
+    $mem_exp = $row["membership_expire_person"];
+  }
+
+
+// make the card
+			print "
+					<section id=\"side1\">
+					<img src=\"ram.jpg\">
+					$itl The Society for Creative Anachronism <br/>
+					Combat Authorization Card <br />
+					Kingdom of Gleann Abhann $itlx <br /> <br />
+					<b>Sca Name: $sca_name </b><br />
+					Mundane Name: $mundane_name <br>
+					Auth Exp: $expire_authorize &nbsp; &nbsp; &nbsp; Marshal Exp: $expire_marshal<br>
+					<span id=\"spacing\">
+					Signature: ______________________________ </span><br />
+					<span class=\"small\"> This your authorization to participate on the field at SCA activities. It must be
+presented to the List Officials at SCA events to register for participation and you may be
+requested to show it to the marshals at any time. </span>
+					</sction>
+					<section id=\"side2\">
+						<div id=\"leftcol\">
+							<b>Authorized Styles:</b> <br />";
+							if (!$authorizations)
+              {
+                echo "None.";
+              }
+              foreach ($authorizations as $auth)
+              {
+                if ($auth != NULL)
+                {
+                  echo $auth . ', ';
+                }
+              }
+						echo "</div>
+					<div id=\"rightcol\">
+						<b>Warrants:</b> <br />";
+            if (!$warrants)
+            {
+              echo "None.";
+            }
+              foreach ($warrants as $warrant)
+                {
+                  if ($warrant != NULL)
+                  {echo $warrant . ', ';}
+                }
+            echo "
+
+					</div>
+					</section>
+			";
+
+
+
+// legacy - candidate for deletion
+//	if (!$found) {
+//		print "<h2>Membership number not found in authorization database.</h2>";
+//	}
+
+?>
+<a id="back" href="combat.php">Back to the Combat Page!</a>
+
+</body>
+
+</html>
