d04775304b1f0655765171dcc64b61fdf3dcee92
Jacqueline Arsenault <justcordelya@gmail.com>
2016-05-06 23:00:51 -0500

    Style for Auth Cards, now with two columns on side 2
    Incorporated style changes from the temporary combat_auth_styled.php
    into combat_auth.php. Massaged the CSS until the columns behaved as
    desired. Card width increased a bit to accommodate that 2nd side right
    column. Column widths adjusted to 45/55% to accommodate the right side
    column. The css file is actually included in this commit.

--- public/combat_auth.php
+++ public/combat_auth.php
@@ -1,16 +1,27 @@
+<!doctype html>
+<META HTTP-EQUIV="CACHE-CONTROL" CONTENT="NO-CACHE">
+<meta http-equiv="expires" content="0" />
+<html lang="en">
+<head>
+  <title>  </title>
+  <link rel="stylesheet" href="/css/auth.css">
+</head>
+<body>
 <?php
 // NOTE: DO NOT AT THIS POINT USE ANY CLASS REFERENCES
 // configuration
-// First: confirm that we reache this page through a POST submission
+
 require("../includes/config.php");
 echo form_title("Fighter Authorization Card");
-// First: confirm that we reache this page through a POST submission
+echo "<div><p><a href=\"combat.php\">Return to the Combat Page</a></p></div>";
+
+// First: confirm that we reached this page through a POST submission
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     if (isset($_POST["mem_num"]) && is_numeric($_POST["mem_num"])) {
          $mem_num = $_POST["mem_num"];
     } else {
-    echo form_subtitle('Must include fighter membership number to print fighter authorization card');  
+    echo form_subtitle('Must include fighter membership number to print fighter authorization card');
     exit_with_footer();
     }
     if (isset($_POST["id_combat"])  && is_numeric($_POST["id_combat"])) {
@@ -19,7 +30,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 } else {
     echo form_subtitle("Page accessed in error.");
     exit_with_footer();
-} 
+}
 
 if (DEBUG) {
     echo form_subsubtitle("Now using values mem_num=$mem_num and id_combat=$id_combat");
@@ -51,9 +62,9 @@ $id_person=$row["id_person"];
 $name_combat=$row["name_combat"];
 //$id_person=764;
 
-$query_fighter="SELECT name_person, name_mundane_person, membership_person, 
-    membership_expire_person, 
-    if(expire_authorize>=NOW(), expire_authorize,'NONE') as expire_authorize, 
+$query_fighter="SELECT name_person, name_mundane_person, membership_person,
+    membership_expire_person,
+    if(expire_authorize>=NOW(), expire_authorize,'NONE') as expire_authorize,
     if(expire_marshal>=NOW(), expire_marshal, 'NONE') as expire_marshal
     FROM Persons, Persons_CombatCards
     WHERE Persons.id_person = Persons_CombatCards.id_person
@@ -63,7 +74,7 @@ if (DEBUG) {
     echo "Find fighter query is:$query_fighter<p>";
 }
 $result_fighter = mysqli_query ($cxn, $query_fighter)
-        or die ("Couldn't find fighter information"); 
+        or die ("Couldn't find fighter information");
 $rowf = mysqli_fetch_assoc($result_fighter);
 $SCA_name=$rowf["name_person"];
 $mundane_name=$rowf["name_mundane_person"];
@@ -78,7 +89,7 @@ if (DEBUG) {
     echo "Find authorization query is:$query_auth<p>";
 }
 $result_auth = mysqli_query ($cxn, $query_auth)
-        or die ("Couldn't find authorization information"); 
+        or die ("Couldn't find authorization information");
 $query_marshal="SELECT M.id_marshal, M.name_marshal, M.id_combat, PM.id_person_marshal
     FROM Marshals M LEFT JOIN Persons_Marshals PM
     ON M.id_marshal=PM.id_marshal AND id_person=$id_person
@@ -87,46 +98,40 @@ if (DEBUG) {
     echo "Find marshal query is:$query_marshal<p>";
 }
 $result_marshal = mysqli_query ($cxn, $query_marshal)
-        or die ("Couldn't find marshal warranting information"); 
+        or die ("Couldn't find marshal warranting information");
 
-echo "<table class='table table-bordered'>";
-echo "<tr><td>";
-echo "The Society for Creative Anachronism <br />
-    $name_combat Authorization Card <br />
-    Kingdom of Gleann Abhann<br />";
-echo "<b>SCA Name: $SCA_name </b><br />"
-        . "Mundane Name: $mundane_name <br />"
-        . "Auth Exp: $expire_authorize &nbsp; &nbsp; &nbsp; Marshal Exp: $expire_marshal<br>"
-        . "<span id=\"spacing\">Signature: ______________________________ </span><br />";
-echo "</td></tr>";
-echo "<tr><td>";
-    echo "<table class='table table-bordered'>";
-    echo "<tr>";
-    echo "<td>"; // Authorizations
-        while ($row = mysqli_fetch_assoc($result_auth)){
-            echo "<li>".$row["name_auth"].": ";
-            if ($row["id_person_auth"] == NULL) {
-                echo "NO<br />";
-            } else {
-                echo "YES<br />";
-            }
-        }
-    echo "</td>";
-    echo "<td>"; // Marshals' Warrants
-        while ($row = mysqli_fetch_assoc($result_marshal)){
-            echo "<li>".$row["name_marshal"].": ";
-            if ($row["id_person_marshal"] == NULL) {
-                echo "NO<br />";
-            } else {
-                echo "YES<br />";
-            }
-        }
-    echo "</td>";
-    echo "</tr>";
-    echo "</table>";
+        echo "<section id=\"side1\">";
 
-echo "</td></tr>";
-echo "</table>";
+        echo "<p style=\"text-align: center\"><b>The Society for Creative Anachronism</b><br/>
+            <i>$name_combat Authorization Card <br />
+            Kingdom of Gleann Abhann</i><br /></p>";
+        echo "<b>SCA Name: $SCA_name </b><br />"
+                . "Mundane Name: $mundane_name <br />"
+                . "Auth Exp: $expire_authorize &nbsp; &nbsp; Marshal Exp: $expire_marshal"
+                . "<p class=\"spacing\">Signature: ______________________________ </p>";
+        echo "<span class=\"small\"> This is your authorization to participate on the field at SCA activities. It must be
+        presented to the List Officials at SCA events to register for participation and you may be
+        requested to show it to the marshals at any time. </span>";
+        echo "</section><section id=\"side2\">
+          <div id=\"leftcol\"><b>Authorizations:</b><br/>"; // Authorizations
+                while ($row = mysqli_fetch_assoc($result_auth)){
+                    echo "* ".$row["name_auth"].": ";
+                    if ($row["id_person_auth"] == NULL) {
+                        echo "NO<br/>";
+                    } else {
+                        echo "YES<br/>";
+                    }
+                }
+                echo "</div><div id=\"rightcol\">";
+                echo "<b>Warrants:</b> <br />"; // Marshals' Warrants
+                while ($row = mysqli_fetch_assoc($result_marshal)){
+                    echo "* ".$row["name_marshal"].": ";
+                    if ($row["id_person_marshal"] == NULL) {
+                        echo "NO<br/>";
+                    } else {
+                        echo "YES<br/>";
+                    }
+                }
+
+        echo "</div></section>";
 ?>
-<a id="back" href="combat.php">Back to the Combat Page!</a>
-
