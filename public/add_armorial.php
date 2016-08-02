68f037b4ba75a7b56e00efe8c23a63903d434f74
Jacqueline Arsenault <justcordelya@gmail.com>
2016-08-01 21:38:20 -0500

    Basic form to add armorial images to the db
    Takes Blazon and image file as input and stores all necessary data to
    db. NOTE: image string is base64 encoded because without it the string
    breaks mySQL syntax rules.

---
+++ public/add_armorial.php
@@ -0,0 +1,9 @@
+<?php
+
+    // configuration
+    require("../includes/config.php");
+
+    // render portfolio
+    render("add_armorial.php", ["title" => "Add a New Device or Badge"]);
+
+?>
---
+++ templates/add_armorial.php
@@ -0,0 +1,107 @@
+<?php
+
+
+if (permissions("Herald")<3){
+    // We don't have permission to add images so let's just exit now.
+    echo '<p class="error"> This page has been accessed in error.</p>';
+    exit_with_footer();
+}
+
+
+
+
+if (($_SERVER['REQUEST_METHOD'] == 'POST')  && (permissions("Herald")>=3)){ //portion commented out for testing
+  $cxn = open_db_browse();
+
+    // We have a form submission.
+    // Note: we allow for addition of multiple devices which is why the blank
+    //        form will reappear at the bottom of the page
+
+$uploadOk = 1; // set the "OK" flag to 1. Future IF tests may change it to 0
+
+// declare some variables
+$blazon = sanitize_mysql($_POST["blazon"]);
+$fname = $_FILES["imagefile"]["name"];
+$fsize = $_FILES["imagefile"]["size"];
+$ftype = $_FILES["imagefile"]["type"];
+$imgheight = NULL;
+$imgwidth = NULL;
+$image = NULL;
+
+// Check if image file is an actual image or fake image
+
+    $check = getimagesize($_FILES["imagefile"]["tmp_name"]);
+
+
+    if($check !== false) {
+      $imgwidth = $check[0];
+      $imgheight = $check[1];
+
+        $uploadOk = 1;
+    } else {
+        echo "File is not an image.";
+        $uploadOk = 0;
+    }
+
+    // Check file size
+    if ($_FILES["imagefile"]["size"] > 500000) {
+       echo "<p class='error'>Sorry, your file is too large. Max file size is 500kb</p>";
+       $uploadOk = 0;
+    }
+    if ($imgheight > 300 || $imgwidth > 300){
+      echo "<p class='error'>Sorry, your file exceeds the maximum dimensions of 300x300 pixels.</p>";
+      $uploadOk = 0;
+    }
+    // Allow only certain file formats
+    if($ftype != "image/jpg" && $ftype != "image/png" && $ftype != "image/jpeg"
+    && $ftype != "image/gif" ) {
+        echo "<p class='error'>Sorry, only JPG, JPEG, PNG & GIF files are allowed. The file you attempted to upload is " . var_dump($ftype) . "</p>";
+        $uploadOk = 0;
+    }
+// read the image file into a string
+    $image = file_get_contents($_FILES["imagefile"]["tmp_name"]);
+    $image = base64_encode($image);
+
+    // Check if $uploadOk is set to 0 by an error
+    if ($uploadOk == 0) {
+        echo "Sorry, your file was not uploaded.";
+    // if everything is ok, try to upload file
+    } else {
+
+
+
+    $query = "INSERT INTO armorials VALUES (NULL, '$blazon', '$image', '$fname', $fsize, '$ftype' )";
+
+
+        $result = update_query($cxn, $query);
+        if ($result !== 1) {
+            echo "Error updating record: " . mysqli_error($cxn);
+        } else {
+            echo "Successfully added ". $fname ." to the database<p>";
+            echo button_link("awards.php", "Return to awards page"); // TODO Identify where we should send users with this button
+            echo "or continue adding new devices below<p>";
+        }
+      }
+
+mysqli_close ($cxn);
+}
+?>
+
+<div class='row'><div class='col-md-8 col-md-offset-2'>
+<form action="add_armorial.php" method="post" enctype="multipart/form-data">
+  <?php echo form_title("Adding a New Device or Badge"); ?>
+  <table class='table table-condensed table-bordered'>
+      <tr>
+          <td class="text-right">Blazon<br>(Required)</td>
+          <td><input type="text" name="blazon" id="blazon" size="50" maxlength="128" required>
+          </td>
+      </tr>
+      <tr>
+        <td>Select image file<br>
+        (Max 300x300 pixels, 500 kb)</td>
+        <td><input type="file" name="imagefile" id="imagefile">
+      </tr>
+  </table>
+  <input type="submit" value="Upload Image">
+</form>
+    </div></div>
