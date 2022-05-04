6161e8acec1d86eb5772804b73d9766f36cccb7f
Cordelya Sharpe <justcordelya@gmail.com>
2022-05-04 09:36:27 -0400

    check in constants-example.php

---
+++ includes/constants-example.php
@@ -0,0 +1,61 @@
+<?php
+
+    /**
+     * constants-example.php
+     *
+     * copy this file to constants.php
+     *
+     * Global constants.
+     */
+
+    // Database information for the Hall of Records
+    define("DATABASE", "database");
+    define("PASSWORD", "password");
+    define("SERVER", "localhost");
+    define("USERNAME", "db_username");
+
+    // Information for the host kingdom
+    // deprecated in v1.0, will be removed in subsequent version
+    define("HOST_KINGDOM", "");
+    define("HOST_KINGDOM_ID", "");
+
+    // Information for password security
+    // Populate with at least 32 random characters, excluding spaces
+    // deprecated in v1.0, will be removed in subsequent version
+    define("SALT", "");
+
+    // Invitation key salt - populate with 10-15 random chars
+    define("KEYSALT", "");
+
+    // Invite key expires after days
+    // must be expressed using DateInterval formatting
+    define("INV_EXP", "P7D");
+
+    // Information for website structure
+    // this is the system path to the top level directory
+    // where index.php and README.md reside
+    define("ROOTDIR", "/path/to/index/file/");
+    
+    // Do you want extra info for debugging? Set 1 for yes, 0 for no
+    define ("DEBUG", "1");
+
+    /* Session expiration settings *
+     * MAX_SESSION is the absolute maximum length a session should be 
+     * allowed to continue.
+     *
+     * MAX_INACTIVE is the length of time after which the session is 
+     * destroyed if no activity occurs.
+     * 
+     * SESSION_REFRESH is the interval at which an active session is 
+     * refreshed.
+     *
+     * each setting should be expressed in seconds.
+     */
+     // max time a session should be active, in seconds
+    define("MAX_SESSION", "604800"); //604800 = 7 days
+    
+    // max amount of inacivity before session is expired, in seconds
+    define("MAX_INACTIVE", "7200"); // 7200 = 2 hours
+    define("SESSION_REFRESH", "1800"); // 1800 = 30 minutes
+
+?>
