849b155e331ce6ef69a4eb998da2cc18353800b5
Cordelya Sharpe <justcordelya@gmail.com>
2022-05-16 13:26:22 -0400

    constants-example.php has docblocks
    file-level docblock and definition-level docblocks for each def

--- includes/constants-example.php
+++ includes/constants-example.php
@@ -3,79 +3,138 @@
     /**
      * constants-example.php
      *
-     * copy this file to constants.php
-     *
-     * Global constants.
+     * Global Constants
+     * Copy this file to constants.php and fill in the details where necessary.
      */
 
-    // Database information for the Hall of Records
-    define("DATABASE", "database");
-    define("PASSWORD", "password");
+    /** Database server name */
     define("SERVER", "localhost");
+
+    /** Database name */
+    define("DATABASE", "database");
+
+    /** Database user's username */
     define("USERNAME", "db_username");
 
-    // Information for the host kingdom
-    // deprecated in v1.0, will be removed in subsequent version
-    define("HOST_KINGDOM", "");
-    define("HOST_KINGDOM_ID", "");
+    /** Database user password */
+    define("PASSWORD", "password");
 
-    // Information for password security
-    // Populate with at least 32 random characters, excluding spaces
-    // deprecated in v1.0, will be removed in subsequent version
+    /**
+     * Information for password security
+     *
+     * Populate with at least 32 random characters, excluding spaces
+     * @deprecated in v1.0, will be removed in subsequent version
+     */
     define("SALT", "");
 
-    // Invitation key salt - populate with 10-15 random chars
+    /**
+     * Invitation key salt.
+     *
+     * Populate with 10-20 random chars
+     */
     define("KEYSALT", "");
 
-    // Invite key expires after days
-    // must be expressed using DateInterval formatting
+    /**
+     * Invitation key expiration delay
+     *
+     * For how long should an invitation key be valid?
+     * Must be expressed using DateInterval formatting
+     */
     define("INV_EXP", "P7D");
 
-    // Information for website structure
-    // this is the system path to the top level directory
-    // where index.php and README.md reside
+    /**
+     * Information for website structure
+     *
+     * this is the system path to the top level directory
+     * where index.php and README.md reside
+     */
     define("ROOTDIR", "/path/to/index/file/");
 
-    // Google Maps API Key
-    // In order to load the Event Site(s) Map, a Google Maps API key is
-    // required.
-    // You should limit the scope of the key to calls originating from your
-    // app's domain because this key will be discoverable via examination of
-    // page source.
-    // example define("MAPSAPI", "your-api-key");
+    /**
+     * Google Maps API Key
+     *
+     * In order to load the Event Site(s) Map, a Google Maps API key is
+     * required. You should limit the scope of the key to calls originating
+     * from your app's domain because this key will be discoverable via
+     * examination of page source.
+     *
+     * example:
+     * <code>
+     * <?php
+     * define("MAPSAPI", "paste-your-api-key-here");
+     * ?>
+     * </code>
+     */
     define("MAPSAPI", "");
 
-    // Google Analytics tracking code
-    // Place your Google Analytics tracking code here and it will be inserted
-    // into the GA javascript widget in the footer
-    // example define("ANALYTICS", "your-code-here");
+    /**
+     * Google Analytics tracking code
+     * Place your Google Analytics tracking code here and it will be
+     * inserted into the GA javascript widget in the footer
+     *
+     * example
+     * <code>
+     * <?php
+     * define("ANALYTICS", "your-code-here");
+     * ?>
+     * </code>
+     */
     define("ANALYTICS", "");
 
-    // where to send custom "debug" level messages
-    // will be deprecated in v.2.0 in favor of a logging framework plugin
-    // in order for this to work as set up here, the server
-    // user that is responsible for the web process (ie "www-data")
-    // must be able to write to the directory /var/log/php/ so that it
-    // can create the log file if one doesn't exist.
-    // Don't expose debug log files to /var/www/ if you can help it.
-    define("DEBUG", "1"); // 0 = off, 1 = on
+    /**
+     * Toggles debugging output
+     *
+     * Set to 1 for debugging output
+     * Set to 0 for no debugging output
+     * Will be deprecated in the future
+     */
+    define("DEBUG", "1");
+
+    /**
+     * Debug log location
+     *
+     * where to send custom "debug" level messages will be deprecated in v.2.0
+     * in favor of a logging framework plugin in order for this to work as set
+     * up here, the server user that is responsible for the web process
+     * (ie "www-data") must be able to write to the directory /var/log/php/ so
+     * that it can create the log file if one doesn't exist. Don't expose debug
+     * log files to /var/www/ if you can help it.
+     */
     define("DEBUG_DEST", "/var/log/php/debug.log");
 
-    /* Session expiration settings *
-     * MAX_SESSION is the absolute maximum length a session should be
-     * allowed to continue.
+    /**
+     * Session Maximum Length
      *
-     * MAX_INACTIVE is the length of time after which the session is
-     * destroyed if no activity occurs.
-     *
-     * SESSION_REFRESH is the interval at which an active session is
-     * refreshed.
-     *
-     * each setting should be expressed in seconds.
+     * MAX_SESSION is the absolute maximum length of time, in seconds, a session
+     * should be allowed to continue.
      */
-     // max time a session should be active, in seconds
     define("MAX_SESSION", "604800"); //604800 = 7 days
 
-    // max amount of inacivity before session is expired, in seconds
-    define("MAX_INACTIVE", "7200"); // 7200 = 2 hours
+    /**
+     * Session Maximum Inactivity
+     *
+     * MAX_INACTIVE is the length of time, in seconds, after which the session is
+     * destroyed if no activity occurs.
+     */
+   define("MAX_INACTIVE", "7200"); // 7200 = 2 hours
+
+    /**
+     * Session refresh interval
+     *
+     * SESSION_REFRESH is the interval of time, in seconds, at which an active
+     * session is refreshed.
+     */
     define("SESSION_REFRESH", "1800"); // 1800 = 30 minutes
+
+    /* If you need to add more constants, add them above this line */
+
+    /** Host kingdom name
+     *
+     * install.php will add these at the end of the install process if they are
+     * not already defined. TODO make the install script use them if they are
+     * defined and append them if they are not.
+     *
+     * if you don't use the install script, uncomment these and fill them in
+     * after you add the host kingdom to the database */
+    //define("HOST_KINGDOM", "");
+    //define("HOST_KINGDOM_ID", "");
